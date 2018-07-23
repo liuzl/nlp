0cc40d5637ddc87fee27e4e44834fc1360c3094d
liuzhanliang <zhanliangliu@qq.com>
2018-07-23 10:44:27 +0800

    earley

---
+++ earley/web/js/parser.js
@@ -0,0 +1,220 @@
+// http://cs.stackexchange.com/questions/40965/cfgs-detecting-infinitely-many-derivations-of-a-single-string
+// http://www.cs.laurentian.ca/jdompierre/html/MATH2056E_W2011/cours/s8.4_closures_relations_BW.pdf
+// https://a2c2a.wordpress.com/2014/09/18/implementing-an-earley-parser-that-handles-nullable-grammars-and-draws-all-unique-parse-trees-in-python/
+// http://web.stanford.edu/~crwong/cfg/grammar.html
+// http://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm
+// TODO wrap in closures, don't pollute global namespace
+
+var assert = require('./assert');
+var grammar = require('./grammar');
+require('./grammar.algorithms')(grammar.Grammar);
+
+var parser = {};
+
+var enums = {
+  DISTINCT: {},
+  SIMILAR: {},
+  IDENTICAL: {}, // ie, same rule, index, and predecessor, but different sub-parses
+  PRODUCEONE: {},
+  PRODUCETWO: {},
+  PRODUCEALL: {}
+}
+parser.PRODUCEONE = enums.PRODUCEONE;
+parser.PRODUCETWO = enums.PRODUCETWO;
+parser.PRODUCEALL = enums.PRODUCEALL;
+
+
+
+var DEBUG = false;
+parser.PRODUCECOUNT = enums.PRODUCETWO;
+
+// TODO this is not the best way of doing this.
+NT = grammar.NT;
+T = grammar.T;
+Rule = grammar.Rule;
+Grammar = grammar.Grammar;
+
+
+// library code, woo
+function arraysEqual(a, b) {
+  if(a === b) return true;
+  if(a == null || b == null) return false;
+  if(a.length != b.length) return false;
+  for(var i = 0; i < a.length; ++i) {
+    if(a[i] !== b[i]) return false;
+  }
+  return true;
+}
+
+
+
+
+
+if(DEBUG) {
+  var _id = 0;
+  function id(){
+    return ++_id;
+  }
+}
+
+function State(rule, index, predecessor, backPointers) {
+  if(!(this instanceof State)) return new State(rule, index, predecessor, backPointers);
+  this.rule = rule;
+  this.index = index;
+  this.predecessor = predecessor;
+  this.backPointers = backPointers || [];
+  if(DEBUG) this.id = id();
+  assert(this.index == this.backPointers.length); // honestly could just do away with index at this point
+}
+State.prototype.done = function(){ return this.index === this.rule.production.length; }
+State.prototype.compare = function(other) {
+  if(this.rule === other.rule
+  && this.index === other.index
+  && this.predecessor === other.predecessor) {
+    if(arraysEqual(this.backPointers, other.backPointers)) {
+      return enums.IDENTICAL;
+    }
+    else {
+      return enums.SIMILAR;
+    }
+  }
+  else {
+    return enums.DISTINCT;
+  }
+}
+State.prototype.next = function(){ return this.rule.production[this.index]; } 
+State.prototype.toString = function(){
+  return '(' + (DEBUG?(this.id.toString() + ' '):'') + this.rule.name + ' -> ' + this.rule.production.slice(0, this.index).join('')
+          + '*' + this.rule.production.slice(this.index).join('') + ', ' + this.predecessor.toString() 
+          + (DEBUG?(', [' + this.backPointers.map(function(x){return x===null?'null':x.id.toString();}).join(',') + ']'):'') + ')';
+}
+
+
+
+
+
+
+
+function parse(grammar, str, produceCount) {
+  if(typeof str !== 'string') throw Error('Can\'t parse non-string object ' + (typeof str));
+  var oldProduceCount = parser.PRODUCECOUNT;
+  if(produceCount) {
+    parser.PRODUCECOUNT = produceCount;
+  }
+  
+  var chart = [];
+  for(var i=0; i<=str.length; ++i) chart.push([]);
+  
+  function seen(state, strPos) {
+    var count = 0;
+    for(var i=0; i<chart[strPos].length; ++i) {
+      var equalness = state.compare(chart[strPos][i]);
+      if(equalness == enums.IDENTICAL || (equalness == enums.SIMILAR && parser.PRODUCECOUNT == enums.PRODUCEONE)) { // either we've seen this exact thing before, or we've seen this modulo different parses and don't care about different parses
+        return true;
+      }
+      if(equalness == enums.SIMILAR && parser.PRODUCECOUNT == enums.PRODUCETWO && ++count > 1) { // we've seen something similar and do care
+        return true;
+      }
+    }
+    return false;
+  }
+  
+  function scanner(state, strPos) {
+    if(state.next().equals(T(str[strPos]))) {
+      var newBPs = state.backPointers.slice(0);
+      newBPs.push(null); // terminals do not need backpointers, of course
+      var advanced = State(state.rule, state.index+1, state.predecessor, newBPs);
+      if(!seen(advanced, strPos+1)) {
+        chart[strPos+1].push(advanced);
+      }
+    }
+  }
+  
+  function predictor(state, strPos) {
+    var sym = state.next();
+    for(var i=0; i<grammar.symbolMap[sym.data].rules.length; ++i) {
+      var advanced = State(grammar.symbolMap[sym.data].rules[i], 0, strPos);
+      if(!seen(advanced, strPos)) {
+        chart[strPos].push(advanced);
+      }
+    }
+    
+    // handle silly nullable cornercase: we might need to "re-run" completer for a nullable
+    // if we are predicting that nullable but it's already been processed
+    // given 'nullable' annotation, we could skip this when 'sym' is not nullable
+    for(var i=0; i<chart[strPos].length; ++i) { // can actually abort when we hit current state, but no real need (todo check speedup)
+      var candidate = chart[strPos][i];
+      if(candidate.rule.name === sym.data && candidate.predecessor === strPos && candidate.done()) {
+        var newBPs = state.backPointers.slice(0);
+        newBPs.push(candidate); // 'candidate' is already done
+        var advanced = State(state.rule, state.index+1, state.predecessor, newBPs);
+        if(!seen(advanced, strPos)) {
+          chart[strPos].push(advanced);
+        }
+      }
+    }
+  }
+  
+  function completer(state, strPos) {
+    var thisSym = NT(state.rule.name);
+    for(var i=0; i<chart[state.predecessor].length; ++i) {
+      var prevState = chart[state.predecessor][i];
+      if(!prevState.done() && thisSym.equals(prevState.next())) {
+        var newBPs = prevState.backPointers.slice(0);
+        newBPs.push(state); // just finished 'state'
+        var advanced = State(prevState.rule, prevState.index+1, prevState.predecessor, newBPs);
+        if(!seen(advanced, strPos)) {
+          chart[strPos].push(advanced);
+        }
+      }      
+    }
+  }
+  
+  
+  var startSym = grammar.start;
+  var gammaRule = Rule(['GAMMA'], [NT(startSym)]); // needs a _unique_ identifier. Easiest way: new object
+  chart[0].push(State(gammaRule, 0, 0));
+  
+  for(var i=0; i<=str.length; ++i) {
+    if(DEBUG) console.log('processing position ' + i)
+  
+    for(var j=0; j<chart[i].length; ++j) {
+      var state = chart[i][j];
+      if(DEBUG) console.log('state ', state.toString())
+      if(!state.done()) {
+        if(state.next().type == 'NT') {
+          if(DEBUG) console.log('p')
+          predictor(state, i);
+        }
+        else {
+          if(DEBUG) console.log('s', state.next())
+          scanner(state, i);
+        }
+      }
+      else {
+        if(DEBUG) console.log('c')
+        completer(state, i);
+      }
+    }
+  }
+
+  // done constructing chart; time to find parses
+  var parses = [];
+  for(var i=0; i<chart[str.length].length; ++i) {
+    var state = chart[str.length][i];
+    if(state.rule === gammaRule && state.done()) {
+      parses.push(state);
+    }
+  }
+  if(DEBUG)
+    console.log(parses.length);
+  
+  parser.PRODUCECOUNT = oldProduceCount;
+  return parses;
+}
+
+parser.parse = parse;
+
+
+
+module.exports = parser;
---
+++ earley/web/js/printers.js
@@ -0,0 +1,260 @@
+var INDENT = '  ';
+function subtreePrinter(state, depth) {
+  depth = depth | 0;
+  var prefix = '';
+  for(var i=0; i<depth; ++i) {
+    prefix += INDENT;
+  }
+  console.log(prefix + state.rule)// + ' ' + state.backPointers.length);
+  prefix += INDENT;
+  for(var i=0; i<state.backPointers.length; ++i) {
+    var backPointer = state.backPointers[i];
+    if(backPointer === null) { // ie, terminal
+      console.log(prefix + state.rule.production[i].data); 
+    }
+    else {
+      subtreePrinter(backPointer, depth+1);
+    }
+  }
+}
+
+
+function rewritePrinter(parse) {
+  var str = [parse];
+  
+  function formatIntermediateString(highlightIndex) { // highlightIndex must be a state, not a final symbol
+    var o = '';
+    for(var i=0; i<str.length; ++i) {
+      if(i == highlightIndex) {
+        o += '*' + str[i].rule.name + '*';
+      }
+      else {
+        if(typeof str[i] === 'string') {
+          o += str[i];
+        }
+        else {
+          o += str[i].rule.name;
+        }
+      }
+    }
+    return o;
+  }
+  
+  for(var i = 0; i<str.length; ++i) { // NB: both str.length and i change within the rewrite
+    if(typeof str[i] === 'string') {
+      continue;
+    }
+    
+    var state = str[i];
+    var out = state.rule.toString() + '  |  ';
+    out += formatIntermediateString(i) + '  |  ';
+    
+    var rewritten = [];
+    for(var j=0; j<state.index; ++j) {
+      if(state.rule.production[j].type == 'T') {
+        rewritten.push(state.rule.production[j].data);
+      }
+      else {
+        rewritten.push(state.backPointers[j]);
+      }
+    }
+    str = str.slice(0, i).concat(rewritten).concat(str.slice(i+1));
+    out += formatIntermediateString(-1);
+    console.log(out);
+    --i; // gotta reprocess the index we just rewrote
+  }
+  
+}
+
+
+
+
+// Helper for domRule and domGrammar
+// Returns a span representing a RHS.
+function domProduction(production) {
+  var o = document.createElement('span');
+  if(production.length == 0) {
+    o.appendChild(document.createTextNode('\u025B')); // epsilon
+  }
+  else {
+    for(var i=0; i<production.length; ++i) {
+      if(production[i].type == 'T') {
+        o.appendChild(document.createTextNode(production[i].data));
+      }
+      else {
+        var sp = document.createElement('span');
+        sp.className = 'cfg-symbol';
+        sp.appendChild(document.createTextNode(production[i].data));
+        o.appendChild(sp);
+      }
+    }
+  }
+  return o;
+}
+
+// helper for domPrinter
+// create a DOM node representing the rule. obviously only call in browsers.
+// symbols get class cfg-symbol, the rule itself class cfg-rule.
+function domRule(rule) {
+  var o = document.createElement('span');
+  o.className = 'cfg-rule';
+  
+  var sp = document.createElement('span');
+  sp.className = 'cfg-symbol';
+  sp.appendChild(document.createTextNode(rule.name));
+  o.appendChild(sp);
+  o.appendChild(document.createTextNode(' \u2192 ')); // right arrow
+  
+  o.appendChild(domProduction(rule.production));
+    
+  return o;
+}
+
+// create a DOM table representing the entire parse. obviously only call in browsers.
+function domPrinter(parse) {
+  var str = [parse];
+  
+  function formatIntermediateString(highlightStart, highlightLength) {
+    if(typeof highlightLength !== 'number' || highlightLength < 0) highlightLength = 1;
+    
+    var o = document.createElement('span');
+    c = o;
+    for(var i=0; i<str.length; ++i) {
+      if(i == highlightStart) {
+        c = document.createElement('span');
+        c.className = 'cfg-rewrite';
+        o.appendChild(c);
+      }
+      
+      if(i - highlightStart >= highlightLength) {
+        c = o;
+      }
+      
+      if(typeof str[i] === 'string') {
+        c.appendChild(document.createTextNode(str[i]));
+      }
+      else {
+        var sp = document.createElement('span');
+        sp.className = 'cfg-symbol';
+        sp.appendChild(document.createTextNode(str[i].rule.name));
+        c.appendChild(sp);
+      }
+    }
+    return o;
+  }
+
+  var out = document.createElement('table');
+  out.className = 'cfg-derivations derivations'; // TODO second is for compat
+  out.innerHTML = '<thead><tr><th>Rule</th><th>Application</th><th>Result</th></tr></thead>';
+  
+  
+  // handle GAMMA state specially
+  var row = document.createElement('tr');
+  var cell = document.createElement('td');
+  var sp = document.createElement('sp');
+  sp.className = 'cfg-rule';
+  sp.innerHTML = 'Start \u2192 ' + '<span class="cfg-symbol">' + parse.backPointers[0].rule.name + '</span>';
+  cell.appendChild(sp);
+  row.appendChild(cell);
+
+  cell = document.createElement('td');
+  var sp = document.createElement('span');
+  sp.className = 'cfg-start';
+  sp.appendChild(document.createTextNode('Start'));
+  cell.appendChild(sp);
+  row.appendChild(cell);
+  
+  str = [parse.backPointers[0]]; // ie, start symbol
+  cell = document.createElement('td');
+  cell.appendChild(formatIntermediateString(-1));
+  row.appendChild(cell);
+  
+  out.appendChild(row);
+
+  
+  for(var i = 0; i<str.length; ++i) { // NB: both str.length and i change within the body of the loop
+    if(typeof str[i] === 'string') {
+      continue;
+    }
+    
+    var state = str[i];
+
+    var row = document.createElement('tr');
+    var cell = document.createElement('td');
+    cell.appendChild(domRule(state.rule));
+    row.appendChild(cell);
+  
+    cell = document.createElement('td');
+    cell.appendChild(formatIntermediateString(i));
+    row.appendChild(cell);
+    
+
+    
+    var rewritten = [];
+    for(var j=0; j<state.index; ++j) {
+      if(state.rule.production[j].type == 'T') {
+        rewritten.push(state.rule.production[j].data);
+      }
+      else {
+        rewritten.push(state.backPointers[j]);
+      }
+    }
+    str = str.slice(0, i).concat(rewritten).concat(str.slice(i+1));
+
+    cell = document.createElement('td');
+    cell.appendChild(formatIntermediateString(i, rewritten.length));
+    row.appendChild(cell);
+    out.appendChild(row);
+
+    --i; // gotta reprocess the index we just rewrote
+  }
+  
+  return out;
+}
+
+
+
+function escapeHTML(str) {
+  // not my preferred solution, but whatever.
+  var div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
+
+// create a DOM div representing the entire parse. obviously only call in browsers.
+function domGrammarPrinter(grammar) {
+  var o = document.createElement('div');
+  var line = document.createElement('span');
+  line.innerHTML = 'Start symbol: <span class="cfg-symbol">' + escapeHTML(grammar.start) + '</span>';
+  o.appendChild(line);
+  o.appendChild(document.createElement('br'));
+  
+  for(var i=0; i<grammar.symbolsList.length; ++i) {
+    var sym = grammar.symbolsList[i];
+    line = document.createElement('span');
+    var sp = document.createElement('span');
+    sp.className = 'cfg-symbol';
+    sp.appendChild(document.createTextNode(sym));
+    line.appendChild(sp);
+    line.appendChild(document.createTextNode(' \u2192 '));
+    for(var j=0; j<grammar.symbolMap[sym].rules.length; ++j) {
+      if(j > 0) {
+        line.appendChild(document.createTextNode(' | '));
+      }
+      var rule = grammar.symbolMap[sym].rules[j];
+      line.appendChild(domProduction(rule.production));
+    }
+    o.appendChild(line);
+    o.appendChild(document.createElement('br'));
+  }
+  
+  return o;
+}
+
+
+module.exports = {
+  subtreePrinter: subtreePrinter,
+  rewritePrinter: rewritePrinter,
+  domPrinter: domPrinter,
+  domGrammarPrinter: domGrammarPrinter
+}
